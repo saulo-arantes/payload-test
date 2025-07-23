cffdfba61b4d57e339ba05818aaabebe20f17e2e
saulo <maxter@Appmax-3574.local>
2025-07-23 10:35:20 -0300

    chargeback value object

---
+++ src/Domain/Chargeback.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ChargebackService\Domain;
+
+interface Chargeback
+{
+    public function getReasonCode(): string;
+
+    public function getDescription(): string;
+
+    public function getCardBrand(): string;
+
+    public function getCustomer(): string;
+
+    public function getSite(): string;
+
+    public function getBankIssuer(): string;
+
+    public function getTransactionId(): string;
+
+    public function getDisputeId(): string;
+
+    public function getChargebackDate(): string;
+}
